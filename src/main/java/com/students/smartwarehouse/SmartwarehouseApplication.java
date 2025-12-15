a96f66be501fc08ac5884907f28e7218d33c3913
hasanbyrktr <bayraktarh317@gmail.com>
2025-12-15 22:20:11 +0300

    Initialize Spring Boot project with MySQL configuration

---
+++ src/main/java/com/students/smartwarehouse/SmartwarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.students.smartwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartwarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartwarehouseApplication.class, args);
+	}
+
+}
